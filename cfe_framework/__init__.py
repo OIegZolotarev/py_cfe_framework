1bb11c98bbbb21160487514968f815b4c83fe727
OIegZolotarev <ovzolotarev@gmail.com>
2026-01-15 21:07:25 +0300

    Update __init__.py

--- cfe_framework/__init__.py
+++ cfe_framework/__init__.py
@@ -2,6 +2,9 @@ from .mdclasses.configuration import Configuration, ConfigurationExtensionCompat
 
 from .mdclasses.commonmodule import CommonModule
 from .mdclasses.catalog import Catalog
-
+from .mdclasses.referencedatatype import ModuleKind
+from .mdclasses.commoncommand import CommonCommand
+from .mdclasses.commandgroup import CommandGroup
+from .mdclasses.httpservice import HTTPService
 
 
